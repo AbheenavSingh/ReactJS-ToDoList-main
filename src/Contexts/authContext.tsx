5305fa7d75e953a610f970e555a71fb98b4c6bdc
Abhinav Singh <singhabhinav0110@gmail.com>
2023-11-16 01:05:37 +0530

    first commit

---
+++ src/Contexts/authContext.tsx
@@ -0,0 +1,39 @@
+import React, {useState, createContext} from "react";
+import { ChildrenProps } from "./deleteContext";
+
+export interface UserDataProps{
+    email: string,
+}
+
+export type AuthType = {
+    userData: UserDataProps,
+    setUserData: Function
+}
+
+const AuthContext = createContext<AuthType|null>(null);
+
+export const AuthProvider: React.FC<ChildrenProps> = ({ children }) => {
+    const [userData, setUserData] = useState(() => {
+      const email = localStorage.getItem('@Project:email');
+  
+      if (email) {
+        try {
+          return { email: JSON.parse(email) };
+        } catch (error) {
+          console.error("Error parsing JSON from localStorage:", error);
+          // Handle the error, or provide a default value if parsing fails.
+        }
+      }
+  
+      return { email: "" };
+    });
+  
+    return (
+      <AuthContext.Provider value={{ userData, setUserData }}>
+        {children}
+      </AuthContext.Provider>
+    );
+  }
+  
+
+export default AuthContext;
